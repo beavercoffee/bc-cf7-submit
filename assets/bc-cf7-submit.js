015ef642ed78e28597a302a8f19a48e2450dad4f
vidsoe <labs@vidsoe.com>
2021-06-15 13:27:48 -0600

    BC

--- assets/bc-cf7-submit.js
+++ assets/bc-cf7-submit.js
@@ -15,20 +15,8 @@ if('undefined' === typeof(bc_cf7_submit)){
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        hide_ajax_loader: function(event){
-            var unit_tag = '';
-			unit_tag = event.detail.unitTag;
-            jQuery('#' + unit_tag).find('.ajax-loader').css('visibility', 'hidden');
-        },
-
-    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
         init: function(){
-            jQuery('.wpcf7-form').on({
-				wpcf7mailsent: bc_cf7_submit.show_ajax_loader,
-				wpcf7reset: bc_cf7_submit.hide_ajax_loader,
-                wpcf7submit: bc_cf7_submit.enable_submit,
-			});
+            jQuery('.wpcf7-form').on('wpcf7submit', bc_cf7_submit.enable_submit);
 			jQuery('.wpcf7-form *').on('keydown', bc_cf7_submit.prevent_submit);
             jQuery('.wpcf7-submit').on('click', bc_cf7_submit.disable_submit);
         },
@@ -47,19 +35,6 @@ if('undefined' === typeof(bc_cf7_submit)){
     				}
     				break;
     		}
-        },
-
-    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        show_ajax_loader: function(event){
-            var ajax_loader = '', unit_tag = '';
-			unit_tag = event.detail.unitTag;
-            ajax_loader = jQuery('#' + unit_tag).find('.ajax-loader');
-            jQuery('#' + unit_tag).find('.wpcf7-form').children().not('.wpcf7-response-output').hide().end().end().append(ajax_loader);
-            ajax_loader.css({
-                margin: 0,
-                visibility: 'visible',
-            });
         },
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
